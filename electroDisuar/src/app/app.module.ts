f41deb1a40b8c8d86d2954a7179b6b796426e6a9
Saúl <saul.santana107@alu.ulpgc.es>
2023-05-16 09:46:04 +0100

    fixed header

--- electroDisuar/src/app/app.module.ts
+++ electroDisuar/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HeaderComponent } from './header/header.component';
+import { ProfilePage } from './profile/profile.page';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
