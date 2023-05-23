afc5a0adf5e9f159c358863d8ec0810e3ee7a905
daniDJmax1 <daniDJmax1@gmail.com>
2023-05-23 19:19:53 +0100

    Mejora del Login + cambios del header

--- electroDisuar/src/app/header/header.component.ts
+++ electroDisuar/src/app/header/header.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import UserService from '../services/user.services';
+import { User } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { getAuth } from 'firebase/auth';
+import { Observable, BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent  implements OnInit {
+  dataUser: any;
+  state: any;
+  states: string[] = [];
+  @Input() avs:any;
+  constructor(private service: UserService) { }
 
-  constructor() { }
+  ngOnInit() {
+    console.log(this.state);
+    this.service.loggedIn.subscribe(value => {
+      this.state = this.service.loggedIn.getValue();
+      this.dataUser = this.service.currentUser;
+      console.log(this.dataUser);
+    });
+    console.log(this.state);
+  }
 
-  ngOnInit() {}
-
+  logOut(){
+    this.service.SignOut();
+  }
 }
--- electroDisuar/src/app/services/user.services.ts
+++ electroDisuar/src/app/services/user.services.ts
@@ -6,6 +6,7 @@ import { BehaviorSubject } from "rxjs";
 //import { HeaderComponent }  from "../header/header.component";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import firebase from "firebase/compat";
+import { Router } from '@angular/router';
 //import { User } from 'firebase/compat/auth';
 //import auth  from 'firebase/compat/app';
 
@@ -15,13 +16,18 @@ import firebase from "firebase/compat";
 })
 
 export class UserService{
-
+  public activo: Boolean = false;
+  public currentUser: any;
+  public avs: any;
+  public loggedIn = new BehaviorSubject<boolean>(false);
+  public condicion = new BehaviorSubject<boolean>(false);
     //userData: Observable<firebase.User | null>;
-  constructor(private angularFireAuth: AngularFireAuth) {
+  constructor(private angularFireAuth: AngularFireAuth, private router: Router,) {
     //this.userData = angularFireAuth.authState;
   }
   /* Sign up */
   SignUp(email: string, password: string) {
+    
     this.angularFireAuth
       .createUserWithEmailAndPassword(email, password)
       .then(res => {
@@ -33,17 +39,29 @@ export class UserService{
   }
   /* Sign in */
   SignIn(email: string, password: string) {
+    
     this.angularFireAuth
       .signInWithEmailAndPassword(email, password)
       .then(res => {
         console.log('Successfully signed in!');
+        this.condicion.next(true);
+        this.activo=true;
+        this.currentUser = email;
+        this.avs="in";
+        this.loggedIn.next(true);
+        alert("Login completado");
+        this.router.navigate(['/home']);
       })
       .catch((err: Error) => {
         console.log('Something is wrong:',err.message);
+        this.condicion.next(false);
       });
+    
   }
   /* Sign out */
   SignOut() {
+    this.avs="out";
+    this.loggedIn.next(false);
     this.angularFireAuth.signOut();
   }  
 
